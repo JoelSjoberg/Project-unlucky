d6229486ea92ffed67dd57edd4973c7aaa718b6f
Joel Sjöberg <joelsjoberg3@gmail.com>
2017-09-25 19:28:26 +0300

    Add files via upload

---
+++ FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(playerTransform.position.x, 10, playerTransform.position.z -20);
+	}
+}
