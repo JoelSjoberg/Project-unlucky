212f19f96b214f9f7c09d88e0fe9f24cc41cd0fa
Joel Sjöberg <joelsjoberg3@gmail.com>
2017-10-24 16:01:18 +0300

    Dodge added to player
    The player can now dodge to move faster for a short while

--- Assets/Scripts/PlayerControllerMapTut.cs
+++ Assets/Scripts/PlayerControllerMapTut.cs
@@ -7,24 +7,46 @@ using UnityEngine;
 public class PlayerControllerMapTut : MonoBehaviour {
 
     public float speed;
+    public float evadeTime;
+    public float evadeSpeed = 100;
+    public bool evading = false;
+
     public GunController gun;
 
-	Rigidbody rigidbody;
-	Vector3 velocity;
+    private float movementSpeed;
+    private float timer;
+
+	private Rigidbody rigidbody;
+	private Vector3 velocity;
 
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent<Rigidbody> ();
-
+        movementSpeed = speed;
+        timer = evadeTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * speed;
+		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * movementSpeed;
 
         // Input(Keyboard and Mouse)
         if (Input.GetMouseButtonDown(0)) gun.isFiring = true;
         if (Input.GetMouseButtonUp(0)) gun.isFiring = false;
+        if (Input.GetKeyDown("left shift"))
+        {
+            movementSpeed = evadeSpeed;
+            evading = true;
+        }
+
+        if (evading) timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            evading = false;
+            movementSpeed = speed;
+            timer = evadeTime;
+        }
     }
 
 	void FixedUpdate(){
