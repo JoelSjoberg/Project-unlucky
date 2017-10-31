aaf489d0c07457c8acd1f13e5956af712ac0f8eb
ViktorSjolind <viktor.sjolind@gmail.com>
2017-10-31 20:30:01 +0200

    Start screen menu

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+
+	public void StartGame(string firstLevel){
+		SceneManager.LoadScene (firstLevel);
+		Debug.Log ("Start game pressed");
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+
+}
+
+
