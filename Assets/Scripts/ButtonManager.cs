c78150e5ab1e525a2690369f776f9572881a4366
ViktorSjolind <viktor.sjolind@gmail.com>
2017-11-01 20:26:55 +0200

    Menu scripts

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+
+	public void StartGame(string firstLevel){
+		SceneManager.LoadScene (firstLevel);
+		Debug.Log ("Start game pressed");
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+
+	public void Menu(string menuLevel){
+		SceneManager.LoadScene (menuLevel);
+		Debug.Log ("Menu button pressed");
+	}
+}
+
+
