f5f24d506481e89034b780cea0de588a6f94dedc
Joel Sjöberg <joelsjoberg3@gmail.com>
2017-11-25 22:05:33 +0200

    Small change
    ...Forgot to remove save test case from portal behaviour... is now fixed

--- Assets/Scripts/MapGeneratorScripts/PortalBehaviour.cs
+++ Assets/Scripts/MapGeneratorScripts/PortalBehaviour.cs
@@ -17,7 +17,7 @@ public class PortalBehaviour : MonoBehaviour {
             FindObjectOfType<MapGenerator>().makeDungeon();
             FindObjectOfType<TimeController>().slowDown(2); // slow down time to emphasize transition
             //Instantiate(plane, FindObjectOfType<MapGenerator>().transform.position, Quaternion.identity);
-            FindObjectOfType<GameStateManager>().savePlayer();
+            // FindObjectOfType<GameStateManager>().savePlayer();
             // SceneManager.LoadScene("Level3");
             
         }
