8c96b5d03132316dc4bfc39e5d980822f58f2aa6
ViktorSjolind <viktor.sjolind@gmail.com>
2017-12-06 22:06:15 +0200

    Dialog shown after safe heaven

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -43,7 +43,7 @@ public class ButtonManager : MonoBehaviour {
 			obj.SetActive (false);
 		}
 		playerRef.inSafeHeaven = false;
-		//FindObjectOfType<DialogueManager>().DisplayNextSentence();
+		FindObjectOfType<DialogueManager>().DisplayNextSentence();
 	}
 }
 
