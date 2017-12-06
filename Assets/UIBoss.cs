b6b7dc3e5abc1b695df70fa03bba1d079a9c2f50
ViktorSjolind <viktor.sjolind@gmail.com>
2017-12-06 17:43:55 +0200

    Boss room hide UI

---
+++ Assets/UIBoss.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIBoss : MonoBehaviour {
+
+	public Scene currentScene;
+	public GameObject[] merchantUI;
+
+	// Use this for initialization
+	void Start () {
+		merchantUI = GameObject.FindGameObjectsWithTag ("MerchantUI");
+		currentScene = SceneManager.GetActiveScene ();
+		Debug.Log (currentScene.name);
+		if (currentScene.name.Equals ("BossTheme")) {
+			foreach (GameObject obj in merchantUI) {
+				obj.SetActive (false);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
